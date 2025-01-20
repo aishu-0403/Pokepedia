1dc8be733ff3d80433a8b81db5a081bfedbcd883
aishu-0403 <142115188+aishu-0403@users.noreply.github.com>
2025-01-20 06:42:08 +0530

    Seed.js

---
+++ seed.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const axios = require('axios');
+const Pokemon = require('../models/Pokemon');
+
+mongoose.connect('mongodb://localhost:27017/pokemonDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function seedData() {
+  try {
+    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
+    for (let item of data.results) {
+      const details = await axios.get(item.url);
+      const pokemon = new Pokemon({
+        id: details.data.id,
+        name: details.data.name,
+        type: details.data.types.map((t) => t.type.name),
+        image: details.data.sprites.front_default,
+        height: details.data.height,
+        weight: details.data.weight,
+        stats: {
+          hp: details.data.stats[0].base_stat,
+          attack: details.data.stats[1].base_stat,
+          defense: details.data.stats[2].base_stat,
+          specialAttack: details.data.stats[3].base_stat,
+          specialDefense: details.data.stats[4].base_stat,
+          speed: details.data.stats[5].base_stat,
+        },
+      });
+      await pokemon.save();
+    }
+    console.log('Database seeded successfully');
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+seedData();
